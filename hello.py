1e588b0bb8c0bb752905477c186ee828b88a38d5
Grzesiek <gelomich@gmail.com>
2018-09-13 19:58:04 +0200

    .

--- hello.py
+++ hello.py
@@ -7,5 +7,18 @@ ex = "!"
 sign = hello + " " + world + ex
 print(sign)
 
-name = input("Jak masz na imie? ")
-print(hello + " " + name + ex)
+name = input("Jak masz na imie? ").capitalize()
+# name = name.capitalize()
+# name = name.upper()
+# print(hello.upper() + " " + name.lower() + ex.upper())
+greating = hello.upper() + " " + name.lower() + ex.upper()
+
+print(greating)
+print(greating[6:9])
+print(greating[6:10])
+print(greating[6:])
+print(greating[:10])
+print(greating[:])
+print(greating[:-2])
+
+
