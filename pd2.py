c5314b34bf8c8b02ff8cfa1266087727155eb76d
Grzesiek <gelomich@gmail.com>
2018-09-16 15:04:10 +0200

    .

---
+++ pd2.py
@@ -0,0 +1,12 @@
+int = "Ala ma kota, kot ma Ale"
+
+a = int[0:1] + chr(114) + int[22:] + int[7:8]
+w = chr(119) + int[8:9] + int[1:2] + chr(105)
+p = chr(112) + chr(115) + chr(121) + chr(44) + " " + int[2:3]
+n = chr(110) + int[2:3] + chr(106) + chr(98) + int[2:3] + chr(114) + chr(100) + chr(122) + chr(105) + int[22:] + chr(106)
+b = chr(98) + int[8:9] + int[7:8] + chr(115) + chr(101) + chr(114) + chr(121)
+
+print(a,w,p,n,b)
+
+#print("Arek woli psy, a najbardziej boksery")
+
