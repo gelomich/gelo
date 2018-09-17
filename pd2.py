336416c44df134fa480ba42779849301aa73c84d
Grzesiek <gelomich@gmail.com>
2018-09-17 18:11:01 +0200

    .

--- pd2.py
+++ pd2.py
@@ -1,12 +1,12 @@
-int = "Ala ma kota, kot ma Ale"
+input = "Ala ma kota, kot ma Ale"
 
-l = ord(int[1:2])
+li = ord(input[1:2])  # zamiana litery l z slowa Ala na cyfre w standardzie ASCII
 
-a = int[0:1] + chr(l + 6) + int[22:] + int[7:8]
-w = chr(l + 11) + int[8:9] + int[1:2] + chr(l - 3)
-p = chr(l + 4) + chr(l + 7) + chr(l + 13) + chr(l - 64) + " " + int[2:3]
-n = chr(l + 2) + int[2:3] + chr(l - 2) + chr(l - 10) + int[2:3] + chr(l + 6) + chr(l - 8) + chr(l + 14) + chr(l - 3) + int[22:] + chr(l - 2)
-b = chr(l - 10) + int[8:9] + int[7:8] + chr(l + 7) + chr(l - 7) + chr(l + 6) + chr(121)
+a = input[0:1] + chr(li + 6) + input[22:] + input[7:8]
+w = chr(li + 11) + input[8:9] + input[1:2] + chr(li - 3)
+p = chr(li + 4) + chr(li + 7) + chr(li + 13) + chr(li - 64) + " " + input[2:3]
+n = chr(li + 2) + input[2:3] + chr(li - 2) + chr(li - 10) + input[2:3] + chr(li + 6) + chr(li - 8) + chr(li + 14) + chr(li - 3) + input[22:] + chr(li - 2)
+b = chr(li - 10) + input[8:9] + input[7:8] + chr(li + 7) + chr(li - 7) + chr(li + 6) + chr(121)
 
 print(a, w, p, n, b)
 
