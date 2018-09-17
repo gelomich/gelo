ff5d70294584aec7e58a5d6cdeafb496c7da477d
Grzesiek <gelomich@gmail.com>
2018-09-17 17:47:51 +0200

    .

--- pd2.py
+++ pd2.py
@@ -1,10 +1,12 @@
 int = "Ala ma kota, kot ma Ale"
 
-a = int[0:1] + chr(114) + int[22:] + int[7:8]
-w = chr(119) + int[8:9] + int[1:2] + chr(105)
-p = chr(112) + chr(115) + chr(121) + chr(44) + " " + int[2:3]
-n = chr(110) + int[2:3] + chr(106) + chr(98) + int[2:3] + chr(114) + chr(100) + chr(122) + chr(105) + int[22:] + chr(106)
-b = chr(98) + int[8:9] + int[7:8] + chr(115) + chr(101) + chr(114) + chr(121)
+l = ord(int[1:2])
+
+a = int[0:1] + chr(l + 6) + int[22:] + int[7:8]
+w = chr(l + 11) + int[8:9] + int[1:2] + chr(l - 3)
+p = chr(l + 4) + chr(l + 7) + chr(l + 13) + chr(l - 64) + " " + int[2:3]
+n = chr(l + 2) + int[2:3] + chr(l - 2) + chr(l - 10) + int[2:3] + chr(l + 6) + chr(l - 8) + chr(l + 14) + chr(l - 3) + int[22:] + chr(l - 2)
+b = chr(l - 10) + int[8:9] + int[7:8] + chr(l + 7) + chr(l - 7) + chr(l + 6) + chr(121)
 
 print(a, w, p, n, b)
 
