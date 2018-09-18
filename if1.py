bf0d8fb392e9f434dd999446fb8cbacd5d8c4eae
Grzesiek <gelomich@gmail.com>
2018-09-18 19:54:42 +0200

    .

--- if1.py
+++ if1.py
@@ -1,12 +1,15 @@
 # sprawdz czy podana liczba jest podzielna przez 3, 5 lub 7
 liczba = input("Podaj liczbe ")
 
+# sprawdzenie czy liczba to napewno liczba
 if liczba.isnumeric():
-    liczba = int(liczba)
+    liczba = int(liczba)    # rzutowanie
 else:
     print("Bledna wartosc")
     exit()
 
+# wlasciwy kod
+# liczba podzielna przez ... jak reszta rowna 0 to jest podzielna ( == 0 )
 if liczba % 3 == 0 or liczba % 5 == 0 or liczba % 7:
     print("Liczba jest podzielna przez 3, 5 lub 7")
 else:
