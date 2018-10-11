a2c6d06bbaa41416ff8d663ea53418cc8ad7805b
Grzesiek <gelomich@gmail.com>
2018-10-11 19:39:33 +0200

    .

--- Day9.py
+++ Day9.py
@@ -16,11 +16,18 @@ class Samochod(object):
         print(f"{self.marka} ruszyl")
 
     def zatrzymaj(self):
-        self.jedzie = False
-        print(f"{self.marka} zatrzymal sie")
+        if self.jedzie == False:
+            print('Samochod już stoi!')
+        else:
+            self.jedzie = False
+            print(f"{self.marka} zatrzymal sie")
 
 auto1 = Samochod('Fiat', 'Panda')
 auto2 = Samochod('Skoda', 'Fabia', 'zielona')
+
+auto2.ruszaj()
+auto2.zatrzymaj()
+auto2.zatrzymaj()
 
 print(vars(auto1))
 print(vars(auto2))
